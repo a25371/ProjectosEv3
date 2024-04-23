0ae9090d0c83f776220f911abc6ced44291c26c7
TheToaster <a25371@svalero.com>
2024-04-23 20:01:40 +0200

    Fragmento2

---
+++ Cuaderno2/MiSegundoFragmento/app/src/main/java/com/example/misegundofragmento/Fragmento1.java
@@ -0,0 +1,53 @@
+package com.example.misegundofragmento;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Fragmento1 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Fragmento1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View vista = inflater.inflate(R.layout.fragment_fragmento1, container, false);
+        if(vista!=null){
+            TextView caja = (TextView) vista.findViewById(R.id.caja1);
+        }
+        return vista;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
--- Cuaderno2/MiSegundoFragmento/app/src/main/java/com/example/misegundofragmento/MainActivity.java
+++ Cuaderno2/MiSegundoFragmento/app/src/main/java/com/example/misegundofragmento/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.misegundofragmento;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnChange;
+    private boolean pantalla = false;
+    final Fragmento1 fragmento1 = new Fragmento1();
+    final Fragmento2 fragmento2 = new Fragmento2();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        inicializarComponentes();
+
+        FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+
+            Fragmento1 fragmento1 = new Fragmento1();
+            ft.add(R.id.fragmento, fragmento1);
+            ft.commit();
+
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
+                if(pantalla){
+                    ft.replace(R.id.fragmento, fragmento2);
+                    pantalla = false;
+                }else{
+                    ft.replace(R.id.fragmento, fragmento1);
+                    pantalla = true;
+                }
+                ft.addToBackStack(null);
+                ft.commit();
+
+            }
         });
+    }
+    private void inicializarComponentes(){
+        btnChange = (Button) findViewById(R.id.btnChange);
     }
 }
